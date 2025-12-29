f142b0b07461dc34dde5cd4640e49d26851766dc
Muhammad Waqas <waqas4346@gmail.com>
2025-12-29 14:32:40 +0500

    added changes in product page

---
+++ assets/personalise-button.js
@@ -0,0 +1,27 @@
+import { Component } from '@theme/component';
+
+/**
+ * Component to handle personalise button clicks and open the modal
+ */
+export class PersonaliseButtonComponent extends Component {
+  requiredRefs = ['personaliseButton'];
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  /**
+   * Opens the personalise modal
+   * @param {Event} event - The click event
+   */
+  openPersonaliseModal = (event) => {
+    event.preventDefault();
+    const dialog = document.querySelector('personalise-dialog');
+    if (dialog) {
+      dialog.showDialog();
+    }
+  };
+}
+
+customElements.define('personalise-button', PersonaliseButtonComponent);
+
